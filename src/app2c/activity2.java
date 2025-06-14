01c897e6515d3600dba714b672c9dc622340cef0
sirMikeee <mike@stcecilia.edu.ph>
2025-06-14 01:29:19 -0700

    added activity 2

---
+++ src/app2c/activity2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app2c;
+
+/**
+ *
+ * @author SCC_PC
+ */
+public class activity2 {
+    
+}
