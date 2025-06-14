254bc016250ead69080933528efef72caf325279
sirMikeee <mike@stcecilia.edu.ph>
2025-06-14 01:23:51 -0700

    added files in first push

---
+++ src/app2c/activity1.java
@@ -0,0 +1,15 @@
+package app2c;
+
+import java.util.Scanner;
+public class activity1 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.print("Enter you name: ");
+        name = sc.nextLine();
+        
+        System.out.println("Hello "+name+", how are you!");
+    }
+    
+}
