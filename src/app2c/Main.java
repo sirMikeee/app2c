3eddecaffac7622336d1f21349847d2c2343eeb1
sirMikeee <mike@stcecilia.edu.ph>
2025-08-08 14:08:06 +0800

    added register

--- src/app2c/Main.java
+++ src/app2c/Main.java
@@ -6,8 +6,14 @@ import banking.bankApp;
 
 public class Main {
     
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int response;
+        bankApp bapp[] = new bankApp[99];
+        bapp[0] = new bankApp();
+    do{ 
+    
         System.out.println("WELCOME TO MY SYSTEM!");
         System.out.println("What do you fell like doing today?");
         System.out.println("1. Banking");
@@ -16,24 +22,63 @@ public class Main {
         System.out.print("Enter your choice: ");
         int choice = sc.nextInt();
         
+        
             switch(choice){
-                
                 case 1:
-                    
-                        System.out.print("Enter Account No.: ");
-                        int accn = sc.nextInt();
-                        System.out.print("Enter Pin No.: ");
-                        int pin = sc.nextInt();
+                    int accn, pin;
+                    System.out.println("1. Register");
+                    System.out.println("2. Login");
+                    System.out.println("3. Exit");
+                    System.out.print("Enter Action: ");
+                    int action = sc.nextInt();
                         
-                        bankApp bapp = new bankApp();
-                        
-                            if(bapp.verifyAccount(accn, pin)){
+                        switch(action){
+                            
+                            case 1:
+                                
+                                
+                                System.out.println("Enter Account No.: ");
+                                bapp[0].setAccNo(sc.nextInt());
+                                System.out.print("Enter Pin No.: ");
+                                bapp[0].setPin(sc.nextInt());
+                                
+                                System.out.println("Registered Accout No: "+bapp[0].getAccNo());
+                                
+                                
+                                break;
+                            case 2:
+                                System.out.print("Enter Account No.: ");
+                                accn = sc.nextInt();
+                                System.out.print("Enter Pin No.: ");
+                                pin = sc.nextInt();
+
+                                
+                                int attempts = 3;
+                                
+                                while(!(bapp[0].verifyAccount(accn, pin))){
+                                    attempts--;
+                                    System.out.println("Inalid account, Attempts Left: "+attempts);
+                                    if(attempts == 0){
+                                        System.out.println("MAX ATTEMPT LIMIT REACHED!");
+                                        System.exit(0);
+                                    }
+                                        System.out.print("Enter Account No.: ");
+                                        accn = sc.nextInt();
+                                        System.out.print("Enter Pin No.: ");
+                                        pin = sc.nextInt();
+                                }
+
                                 System.out.println("LOGIN SUCCESS!");
-                            }else{
-                                System.out.println("LOGIN FAILED!");
-                            }
-                    
-                    
+                                
+                                
+                            break;
+                            case 3:
+                                //EXIT
+                                break;
+                            default:
+                                System.out.println("INVALID ACTION");
+                        
+                        }
                     break;
                 case 2:
                     break;
@@ -43,8 +88,10 @@ public class Main {
                     System.out.println("INVALID ACTION!");
             
             }
-        
-        
+        System.out.println("Continue? (1/0): ");
+        response = sc.nextInt();
+    }while(response == 1);
+    
     }
     
 }
--- src/banking/bankApp.java
+++ src/banking/bankApp.java
@@ -3,8 +3,35 @@ package banking;
 
 public class bankApp {
     
-    int accNo = 100012, pin = 1234;
-    float balance;
+    private int accNo, pin;
+    private float balance;
+
+    public void setAccNo(int accNo) {
+        this.accNo = accNo;
+    }
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+    
+    
+    
     
     public boolean verifyAccount(int acc, int pn){
         if(accNo == acc && pin == pn){
